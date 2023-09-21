439ee17c97143e547469e76f1a32e18c91bf0765
Vamsi Krishna <96541358+iam-orsu@users.noreply.github.com>
2023-09-21 07:01:30 +0530

    Create enable.ps1

---
+++ enable.ps1
@@ -0,0 +1,4 @@
+Get-LocalUser -Name "Administrator" | Enable-LocalUser
+$UserAccount = Get-LocalUser -Name "Administrator"
+$UserAccountPassword = "PutYourPasswordYouWantHere" | ConvertTo-SecureString -asPlainText -Force
+$UserAccount | Set-LocalUser -Password $UserAccountPassword
